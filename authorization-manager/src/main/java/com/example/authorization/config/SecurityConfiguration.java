b26d3199e7aea61ddb561762617b32e23a91f226
wujinwang <80656252+wujinwang@users.noreply.github.com>
2023-04-10 18:51:25 -0700

    1

--- authorization-manager/src/main/java/com/example/authorization/config/SecurityConfiguration.java
+++ authorization-manager/src/main/java/com/example/authorization/config/SecurityConfiguration.java
@@ -16,13 +16,30 @@
 
 package com.example.authorization.config;
 
+import static org.springframework.beans.factory.config.BeanDefinition.ROLE_INFRASTRUCTURE;
+
+import java.util.Collection;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.JdkRegexpMethodPointcut;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.authorization.method.AuthorizationManagerBeforeMethodInterceptor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.authorization.security.CustomAuthorizationManager;
+import com.example.authorization.service.CustomUserDetailService;
 
 /**
  * An example of explicitly configuring Spring Security with the defaults.
@@ -34,11 +51,45 @@ import com.example.authorization.security.CustomAuthorizationManager;
 public class SecurityConfiguration {
 
 	@Bean
+	public AuthenticationManager authenticationManager(HttpSecurity httpSecurity, UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
+		AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+		return authenticationManagerBuilder.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+		return new CustomUserDetailService(bCryptPasswordEncoder);
+	}
+
+	@Bean
+	public AuthorizationManager<Collection<ConfigAttribute>> authorizationManager() {
+		return new CustomAuthorizationManager<Collection<ConfigAttribute>>();
+	}
+
+	@Bean
+	@Role(ROLE_INFRASTRUCTURE)
+	public Advisor authorizationManagerBeforeMethodInterception(AuthorizationManager<MethodInvocation> authorizationManager) {
+		JdkRegexpMethodPointcut pattern = new JdkRegexpMethodPointcut();
+		pattern.setPattern("com.baeldung.enablemethodsecurity.services.*");
+		return new AuthorizationManagerBeforeMethodInterceptor(pattern, authorizationManager);
+	}
+
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		System.out.println("-----securityFilterChain----");
 		// @formatter:off
-		http.authorizeHttpRequests((authorize) -> authorize.requestMatchers("/my/authorized/endpoint").access(new CustomAuthorizationManager()));
+		//http.authorizeHttpRequests((authorize) -> authorize.requestMatchers("/my/authorized/endpoint").access(new CustomAuthorizationManager()));
 
+		 http.csrf()
+         .disable()
+         .authorizeHttpRequests()
+         .anyRequest()
+         .authenticated()
+         .and()
+         .sessionManagement()
+         .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		 
 		// @formatter:on
 		return http.build();
 	}
--- authorization-manager/src/main/java/com/example/authorization/security/CustomAuthorizationManager.java
+++ authorization-manager/src/main/java/com/example/authorization/security/CustomAuthorizationManager.java
@@ -1,25 +1,38 @@
 package com.example.authorization.security;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.function.Supplier;
 
-import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.authentication.AuthenticationTrustResolver;
 import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 
-public class CustomAuthorizationManager<T> implements AuthorizationManager<MethodInvocation> {
+public class CustomAuthorizationManager<T> implements AuthorizationManager<Collection<ConfigAttribute>> {
 	private final AuthenticationTrustResolver trustResolver = new AuthenticationTrustResolverImpl();
 
 	@Override
-	public AuthorizationDecision check(Supplier<Authentication> authentication, MethodInvocation methodInvocation) {
+	public AuthorizationDecision check(Supplier<Authentication> authentication, Collection<ConfigAttribute> collection) {
 
-		
 		if (hasAuthentication(authentication.get())) {
-			return new AuthorizationDecision(true);
+			Iterator<ConfigAttribute> iterator = collection.iterator();
+			while (iterator.hasNext()) {
+				ConfigAttribute ca = iterator.next();
+				// required permission of this request
+				String needRole = ca.getAttribute();
+				// all permissions of current user
+				Collection<? extends GrantedAuthority> authorities = authentication.get().getAuthorities();
+				for (GrantedAuthority authority : authorities) {
+					if (authority.getAuthority().equals(needRole)) {
+						return new AuthorizationDecision(true);
+					}
+				}
+			}
 		}
-
 		return new AuthorizationDecision(false);
 	}
 
